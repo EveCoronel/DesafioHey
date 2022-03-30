6991a19accf9bddc731bc87045308a7751450cc9
EveCoronel <evelyncoronel05@gmail.com>
2022-03-30 01:00:48 -0300

    Se agrega contenido

--- app.js
+++ app.js
@@ -10,14 +10,46 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const mensajeInicio = ('Bienvenido a la heladería IceCream Bot, nos alegra que te hayas comunicado. Para realizar tu pedido ingresa "/helado"')
 
 let sabores = ['Frutilla', 'Frambuesa', 'Chocolate', 'Menta', 'Sambayon', 'Maracuya', 'ChocolateItaliano', 'Limon', 'Crema', 'Cafe']
-// function inicioHeladeria(ctx) {}
+let inicio = ['Hola', 'Buenas tardes', 'hola', 'ola', 'Quisiera hacer un pedido', 'Era para hacer un pedido', 'pedir', 'helado', 'Buenas', 'buenos días']
 
-bot.hears('Hola', (ctx) => {
-    // inicioHeladeria()
-    ctx.reply(mensajeInicio)
+// Inicio Heladería IceCream Bot
+
+bot.command('start', (ctx) => {
+
+    ctx.reply(`Bienvenido ${ctx.from.first_name} a nuestra heladería IceCream Bot, si deseas realizar un pedido puedes seguir los paso a continuación.\n
+    1) Verifica nuestro /Catalogo en busca del helado que más gustes\n
+    2) Realiza la selección de sabores ingresando /Helado \n
+    3) Ingresa la dirección a la que deseas el pedido \n
+    4) Ingresa el método de pago que desees \n
+    5) Finalmente confirma tu pedido`)
+
 })
 
-bot.command('helado', (ctx) => {
+bot.hears(inicio, (ctx) => {
+
+    ctx.reply('Gracias por volver a comuicarte con IceCream Bot, puedes realizar tu pedido ingresando /start')
+
+})
+
+bot.command('Catalogo', (ctx) => {
+
+    ctx.replyWithPhoto({
+        source: './media/catalogo.png',
+        
+    })
+
+    ctx.reply('/1Litro /2Litros /3Litros')
+
+})
+
+let litros = ['1Litro', '2Litros', '3Litros']
+
+bot.command(litros, (ctx) => {
+
+ctx.reply(`¡Perfecto! Tu pedido será de ${ctx.message.text}, ahora debes seleccionar los sabores ingresando /Helado`)
+})
+
+bot.command('Helado', (ctx) => {
 
     ctx.reply('Aquí puedes elegir los sabores que tú deseas, luego de seleccionarlos cliquea en "Continuar"')
 
@@ -31,34 +63,40 @@ bot.command(sabores, (ctx) => {
 })
 
 
-bot.command('Continuar', (ctx) =>{
+bot.command('Continuar', (ctx) => {
 
     ctx.reply('Muy bien, ahora escribe la dirección a la que será enviado tu pedido')
 });
 
+bot.hears('Dirección', (ctx) => {
 
-
-
-
-
-/*
-
-bot.action('enviarSabor', (ctx) => {
-
-    ctx.reply('Frutilla')
-})
-
-
-bot.on('text', (ctx) => {
-    // Explicit usage
-    ctx.replyWithPhoto({
-        url: 'https://placekitten.com/g/200/300?random',
-        filename: 'kitten.jpg'
-      })
-   // ctx.telegram.sendMessage(ctx.message.chat.id, `Hola ${ctx.from.first_name}`)
+    ctx.reply(`La dirección de envío será "${ctx.message.text}" ingresa /OK para confirmar o escribe una nueva dirección`)
 
 })
-*/
+
+bot.command('OK', (ctx) => {
+    ctx.reply('Genial, ya casi finalizas tu pedido, solo debes seleccionar el método de pago que tú desees, /Efectivo, /Tarjeta')
+})
+
+bot.command('Efecivo', (ctx => {
+    ctx.reply('Perfecto el pago será en efectivo, confirma la orden ingresando /Confirmar, de lo contrario podrás /Salir')
+}))
+
+
+bot.command('Tarjeta', (ctx => {
+    ctx.reply('Perfecto el pago será con tarjeta, confirma la orden ingresando /Confirmar, de lo contrario podrás /salir')
+}))
+
+bot.command('Confirmar', (ctx => {
+    ctx.reply('Muchas gracias por tu pedido, ¡Esperamos que lo disfrutes!')
+}))
+
+bot.command('salir', (ctx) => {
+
+    ctx.reply(`Adios ${ctx.from.first_name}, ¡Esperamos verte pronto!`)
+
+})
+
 
 
 bot.launch()
